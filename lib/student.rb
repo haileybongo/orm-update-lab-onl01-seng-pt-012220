4a8d71f4cd038e41abc66457d24ae47a0bdc4128
Hailey Bongo <haileyebongo@gmail.com>
2020-04-02 04:18:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,13 +36,12 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
   def self.create(name, grade)
     new_student = self.new(name, grade)
     new_student.save
-
   end
   
   def self.new_from_db(row)
