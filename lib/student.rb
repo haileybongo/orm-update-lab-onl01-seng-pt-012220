bcd30e304d8e6000d2c5530857e6e50da2bc8d45
Hailey Bongo <haileyebongo@gmail.com>
2020-04-02 04:22:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,7 @@ class Student
   
   def update
     sql = "UPDATE songs SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.album, self.id)
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 end
